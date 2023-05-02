5f6e9c3b807d5372dcd900c2df8b2940735772da
glazimirmalevich <torekhan.olzhas123@gmail.com>
2023-05-02 17:59:04 +0600

    Fixed backward class removing

--- index.js
+++ index.js
@@ -6,25 +6,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
   menu.addEventListener("click", (event) => {
     event.preventDefault();
+    // if (event.target.tagName === "A") {
+    //     menuItems.forEach((item) => {
+    //         tabItems.forEach((element) => {
+    //             if(item.innerText === event.target.innerText && element.innerText.includes(item.innerText.slice(8)) ){
+    //                 item.classList.add("active");
+    //                 element.classList.add("active");
+    //                 console.log(item.classList);
+    //                 console.log(element.classList);   
+    //             }
+    //             else if(item.classList.contains("active") && element.classList.contains("active")){
+    //                 item.classList.remove("active");
+    //                 element.classList.remove("active");
+    //                 console.log(item.classList);
+    //                 console.log(element.classList);   
+    //             }
+    //         });
+    //     });
+    // }
     if (event.target.tagName === "A") {
-        menuItems.forEach((item) => {
-            tabItems.forEach((element) => {
-                if(item.innerText === event.target.innerText && element.innerText.includes(item.innerText.slice(8)) ){
-
-                    item.classList.add("active");
-                    element.classList.add("active");
-                    console.log(item.classList);
-                    console.log(element.classList);   
-                }
-                else if(item.classList.contains("active") && element.classList.contains("active")){
-                    item.classList.remove("active");
-                    element.classList.remove("active");
-                    console.log(item.classList);
-                    console.log(element.classList);   
-                }
-            });
-        });
-    }
+        for (let i = 0; i < menuItems.length; i++) {
+          const menuItem = menuItems[i];
+          const tabItem = tabItems[i];
+          if (menuItem === event.target) {
+            menuItem.classList.add("active");
+            tabItem.classList.add("active");
+          } else {
+            menuItem.classList.remove("active");
+            tabItem.classList.remove("active");
+          }
+        }
+      }
     // if (event.target.tagName === "A") {
     //     for(let [key, value] of Object.entries(event.target.parentElement.children)){
     //         if(event.target.innerText === value.innerText){
